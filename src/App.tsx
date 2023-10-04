19e1b99ac9583863296292177d94fb0b7fc47a6d
Evgeniy <nikolaev_zheka-zik@bk.ru>
2023-10-04 17:13:24 +0300

    add basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const wordsArray = [
-  "программа",
+  "Программа",
   "макака",
   "прекрасный",
   "оладушек",
@@ -12,17 +12,34 @@ const wordsArray = [
   "винишко",
   "музыка",
   "обладатель",
-  "планетарный"
+  "планетарный",
+  "Коновал",
+  "Склочник",
+  "Своевременный",
+  "Проявить",
+  "Лукавый",
+  "Расщедриться",
+  "Большак",
+  "Тронуться",
+  "Поклевать",
+  "Голубчик",
+  "Разукомплектовать",
+  "Панты",
+  "Болезненный",
+  "Циркорама",
 ];
 
-const randomWord = Array.from(wordsArray[Math.floor(Math.random() * wordsArray.length)])
-const answerArray = randomWord.map((i) => i = '_ ')
+const MAX_MISS = 10
+let remainingMiss = MAX_MISS
 
+const randomWord = Array.from(wordsArray[Math.floor(Math.random() * wordsArray.length)].toLowerCase())
+const answerArray = randomWord.map((i) => i = '_ ')
+let remainingLetters = randomWord.length
 
 const App = () => {
   const [answer, setAnswer] = useState('')
-  let remainingLetters = randomWord.length;
-
+  const [win, setWin] = useState(false)
+  const [loss, setLoss] = useState(false)
 
   const onChange = (e: React.SyntheticEvent<HTMLInputElement>) => {
     setAnswer(e.currentTarget.value)
@@ -30,33 +47,44 @@ const App = () => {
 
   const onConfirm = (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log(e.currentTarget.nodeValue)
     if (answer.length !== 1) {
       console.log('Пожалуйста введите одну букву')
     } else {
-      for (let j = 0; j < randomWord.length; j++) {
-        if (randomWord[j] === answer) {
-          answerArray[j] = answer
-          remainingLetters--
-        }
-      }
-    }
 
-    console.log(answerArray)
+      if (randomWord.join('').indexOf(answer.toLowerCase()) >= 0) {
+        for (let j = 0; j < randomWord.length; j++) {
+          if (randomWord[j] === answer.toLowerCase()) {
+            answerArray[j] = answer.toLowerCase() + ' '
+            remainingLetters--        
+          }
+        }
+      } else {
+        remainingMiss--
+      }
+
+      setAnswer('')
+    }
   }
 
-
-
+  useEffect(() => {
+    remainingMiss === 0 && setLoss(true)
+    remainingLetters === 0 && setWin(true)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [remainingMiss, remainingLetters])
+  
   return (
     <div>
       <div>Виселица!</div>
-      <div>Осталось отгадать {remainingLetters} букв</div>
+      <div>Букв осталось отгадать: {remainingLetters}</div>
       <div>{randomWord}</div>
       <div>{answerArray}</div>
       <form onSubmit={onConfirm}>
-        <input type="text" name="word" id="word" value={answer} maxLength={1} onChange={onChange}/>
+        <input type="text" value={answer} maxLength={1} onChange={onChange}/>
         <button type="submit">Проверить</button>
       </form>
+      <div>Попыток: {remainingMiss} из {MAX_MISS}</div>
+      {loss && <h1>ПОРАЖЕНИЕ</h1>}
+      {win && <h1>ПОБЕДА!!</h1>}
     </div>
   );
 }
